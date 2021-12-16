722b35bd70ed16a4689f4cd2c5978694f76616bd
ScienceTechWorks <sciencetechworks@gmail.com>
2021-12-16 20:40:21 +0100

    Adding Beer Controller

---
+++ src/main/java/com/stw/beerService/web/controller/BeerController.java
@@ -0,0 +1,50 @@
+
+package com.stw.beerService.web.controller;
+
+import com.stw.beerService.web.model.BeerDto;
+import java.util.UUID;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *          ScienceTechWorks
+ * @author Ramon.Talavera@gmail.com 
+ */
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+    
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(
+            @PathVariable("beerId") UUID beerId){
+        return new ResponseEntity<>(BeerDto.builder().build(),HttpStatus.OK);
+    }
+    
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto){
+       return new ResponseEntity(HttpStatus.CREATED); 
+    }
+    
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeerById(
+            @PathVariable("beerId") UUID beerId,
+            @RequestBody BeerDto beerDto){
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+    
+    @DeleteMapping({"/{beerId}"})
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBeer(@PathVariable("beerId") UUID beerId){
+     
+    }
+
+}
