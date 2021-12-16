b21b585ba5f22e2b2fbaefd1dd2c4e5c3b8e969b
ScienceTechWorks <sciencetechworks@gmail.com>
2021-12-16 19:47:59 +0100

    Initial commit

---
+++ src/test/java/com/stw/beerService/BeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stw.beerService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
