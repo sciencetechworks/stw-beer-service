e42f716e886f6c4c727f3b4c81cab1562cf0a62d
ScienceTechWorks <sciencetechworks@gmail.com>
2021-12-21 18:25:05 +0100

    Added Validation to BeerDto.

--- src/main/java/com/stw/beerService/web/model/BeerDto.java
+++ src/main/java/com/stw/beerService/web/model/BeerDto.java
@@ -4,6 +4,10 @@ package com.stw.beerService.web.model;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,19 +22,33 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class BeerDto {
+    
+  @Null //ReadOnly property: Let the system control its value
   private UUID id;
+  
+  @Null
   private Integer version;
   
+  @Null
   private OffsetDateTime createdDate;
+  
+  @Null
   private OffsetDateTime lastModifiedDate;
   
+  @NotBlank
   private String beerName;
   
+  @NotNull
   private BeerStyleEnum beerStyle;
   
+  @Positive
+  @NotNull
   private Long upc;
   
+  @Positive
+  @NotNull
   private BigDecimal price;
+  
   
   private Integer quantityOnHand;
 }
