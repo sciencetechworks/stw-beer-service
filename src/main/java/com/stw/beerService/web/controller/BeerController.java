ef439ba6404bbbda3edae8659dfe453c1e1aaef0
ScienceTechWorks <sciencetechworks@gmail.com>
2021-12-23 21:24:56 +0100

    Prepared for ASCIIDOCTOR,but document generation is disabled.

--- src/main/java/com/stw/beerService/web/controller/BeerController.java
+++ src/main/java/com/stw/beerService/web/controller/BeerController.java
@@ -9,32 +9,26 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 /**
- *          ScienceTechWorks
- * @author Ramon.Talavera@gmail.com 
+ * Created by jt on 2019-05-12.
  */
 @RequiredArgsConstructor
-@RestController
 @RequestMapping("/api/v1/beer")
+@RestController
 public class BeerController {
-    
+
     private final BeerMapper beerMapper;
     private final BeerRepository beerRepository;
 
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId){
 
-        return new ResponseEntity<>(
-                beerMapper.beerToBeerDto(beerRepository.findById(beerId).get()),
-                HttpStatus.OK);
+        return new ResponseEntity<>(beerMapper.beerToBeerDto(
+                beerRepository.findById(beerId).get()), HttpStatus.OK);
     }
 
     @PostMapping
@@ -58,6 +52,5 @@ public class BeerController {
 
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
-
 
 }
